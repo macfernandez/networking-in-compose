aef9f066030fba3310741701b3373e123510b881
Macarena Fernandez Urquiza <m.fernandezurquiza@gmail.com>
2023-10-15 04:40:22 -0300

    add convert_timestamp2binary

--- src/app/app/converters.py
+++ src/app/app/converters.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
 from fastapi import APIRouter
+from urllib.parse import quote 
+
 
 router = APIRouter()
 
 
-@router.get("/{binary}")
+@router.get("/unix2datetime/{binary}")
 def convert_binary2timestampp(binary: int):
     stamp = datetime.fromtimestamp(binary)
     timestamp = datetime.strftime(stamp, format="%a %b %d %Y %H:%M:%S")
     return timestamp
+
+@router.get("/datetime2unix/{datetime}")
+def convert_binary2timestampp(date: str):
+    # TODO: decode date
+    date = datetime.datetime.strptime(quote(datetime), "%Y-%m-%d:%H:%M:%S")
+    return date.strftime("%s")
